28c1369382c87b9e14290b77387d1a2194f913ec
andyphancode <andyphanpxh@gmail.com>
2023-05-22 03:36:17 -0700

    Update data.sql
    added seeding for data to industries

--- data.sql
+++ data.sql
@@ -1,7 +1,13 @@
-\c biztime
+\c biztime_test
 
-DROP TABLE IF EXISTS invoices;
-DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS invoices
+CASCADE;
+DROP TABLE IF EXISTS companies
+CASCADE;
+DROP TABLE IF EXISTS industries
+CASCADE;
+DROP TABLE IF EXISTS companies_industries
+CASCADE;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -19,6 +25,17 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+  code text PRIMARY KEY,
+  industry text UNIQUE
+);
+
+CREATE TABLE companies_industries (
+  company_code text NOT NULL REFERENCES companies ON DELETE CASCADE,
+  industry_code text NOT NULL REFERENCES industries ON DELETE CASCADE,
+  PRIMARY KEY(company_code, industry_code)
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -28,3 +45,13 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries 
+  VALUES ('tech', 'Tech'),
+         ('acct', 'Accounting'),
+         ('hosp', 'Hospitality');
+INSERT INTO companies_industries VALUES
+  ('apple','tech'),
+  ('ibm','tech'),
+  ('apple','hosp'),
+  ('ibm', 'acct');
